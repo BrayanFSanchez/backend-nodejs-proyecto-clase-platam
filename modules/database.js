8083450d1fa28e05272e0b5546c0706d6cdcad44
Brayan <brayan_sanchez99@hotmail.es>
2022-08-11 20:54:34 -0600

    Agregando conexion a mongo atlas

--- modules/database.js
+++ modules/database.js
@@ -1,6 +1,18 @@
 var mongoose = require('mongoose');
 
-let bd = 'nombreBase';
+const DB_URI = 'mongodb+srv://Brayan:ms8mZpC2BuvsPU58@api-rest.nue8jbk.mongodb.net/platam?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useFindAndModify: true,
+    //useCreateIndex: true
+})
+.then((db) => console.log('Mongodb se conecto a: ', db.connection.host))
+.catch((err) => console.log(err));
+        
+
+/*let bd = 'nombreBase';
 let port = '27017';
 let host = 'localhost';
 
@@ -17,3 +29,4 @@ class Database{
 }
 
 module.exports = new Database();
+*/
