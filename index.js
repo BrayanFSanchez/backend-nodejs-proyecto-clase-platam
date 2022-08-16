8975a94dac45bc247c40b656b4159266f3675bfd
Brayan <brayan_sanchez99@hotmail.es>
2022-08-15 20:07:40 -0600

    Terminando los servicios web de empresas

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var cors = require('cors');
 var database = require('./modules/database');
 var adminsRouter = require('./routers/admins-router');
 var usuariosRouter = require('./routers/usuarios-router');
+var empresasRouter = require('./routers/empresas-router');
 
 var app = express();
 
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/admins', adminsRouter);
 app.use('/usuarios', usuariosRouter);
+app.use('/empresas', empresasRouter);
 
 app.get('/', function(req, res){
     res.send('Servidor backend en linea');
--- routers/empresas-router.js
+++ routers/empresas-router.js
@@ -2,20 +2,99 @@ var express = require('express');
 var router = express.Router();
 
 //Crear un empresa
+router.post('/', function(req, res){
+    res.send(`Se creo una nueva empresa`);
+    res.end();
+});
+
 //Crear página
+router.post('/:idEmpresa/paginas', function(req, res){
+    res.send(`Se creo una nueva página para la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Eliminar página
+router.delete('/:idEmpresa/paginas/:idPagina', function(req, res){
+    res.send(`Se eliminó la pagina ${req.params.idPagina} de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Actualizar página
+router.put('/:idEmpresa/paginas/:idPagina', function(req, res){
+    res.send(`Se actualizó la pagina ${req.params.idPagina} de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Subir archivo
+router.post('/:idEmpresa/archivos', function(req, res){
+    res.send(`Se creo un nuevo archivo para la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Actualizar archivo
+router.put('/:idEmpresa/archivos/:idArchivo', function(req, res){
+    res.send(`Se actualizó el archivo ${req.params.idArchivo} de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Eliminar archivo
+router.delete('/:idEmpresa/archivos/:idArchivo', function(req, res){
+    res.send(`Se eliminó el archivo ${req.params.idArchivo} de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Guardar bloque de contenido
+router.post('/:idEmpresa/bloques', function(req, res){
+    res.send(`Se guardó un bloque de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Actualizar bloque de contenido
+router.put('/:idEmpresa/bloques/:idBloque', function(req, res){
+    res.send(`Se actualizó el bloque ${req.params.idArchivo} de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Eliminar bloque de contenido
+router.delete('/:idEmpresa/bloques', function(req, res){
+    res.send(`Se eliminó un bloque de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Crear producto
+router.post('/:idEmpresa/productos', function(req, res){
+    res.send(`Se guardó un producto de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Eliminar producto
+router.delete('/:idEmpresa/productos/:idProducto', function(req, res){
+    res.send(`Se eliminó el producto ${req.params.idProducto} de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Actualizar producto
+router.put('/:idEmpresa/productos/:idProducto', function(req, res){
+    res.send(`Se actualizó el producto ${req.params.idProducto} de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Crear categoria
+router.post('/:idEmpresa/categorias', function(req, res){
+    res.send(`Se guardó una categoria de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Eliminar categoria
+router.delete('/:idEmpresa/categorias/:idCategoria', function(req, res){
+    res.send(`Se eliminó la categoria ${req.params.idCategoria} de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
+
 //Actualizar categoria
+router.put('/:idEmpresa/categorias/:idCategoria', function(req, res){
+    res.send(`Se actualizó la categoria ${req.params.idCategoria} de la empresa ${req.params.idEmpresa}`);
+    res.end();
+});
 
 module.exports = router;
